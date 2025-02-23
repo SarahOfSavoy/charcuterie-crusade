e60fa882dee3dd16bccdcfb0891d6ba482521a90
stevenlchow7 <stevenlchow7@gmail.com>
2025-02-23 15:36:15 -0600

    Created basic level
    Created a basic level within the main scene that has a temporary background, platforms, and coins. Added a camera to the player scene to follow the player.

---
+++ globals.gd
@@ -0,0 +1,5 @@
+# This script will be used for any global variables
+extends Node
+
+# Global variable to keep track of the player's score
+var score = 0
--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,14 +1,11 @@
 extends CanvasLayer
 
-# This will be a global variable accessed using HUD.score
-var score = 0
-
 @onready var scoreLabel = get_node("Score")
 
 # Repeatedly update the state of the player (score and health)
 func _process(delta: float) -> void:
 	if scoreLabel != null:
-		update_score(HUD.score)
+		update_score(Globals.score)
 
 # Change the score text with a new score
 func update_score(new_score):
