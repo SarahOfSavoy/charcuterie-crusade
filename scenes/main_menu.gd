c1597208542b57663739c990a6fb454e86d8b65d
stevenlchow7 <stevenlchow7@gmail.com>
2025-02-24 14:27:30 -0600

    Made transparent sprites and added music to main menu
    Sorry for all the import files?

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
 
+func _ready() -> void:
+	$Music.play()
+
+
 # When the user clicks the start button
 func _on_start_button_pressed() -> void:
 	# Change the scene to the main scene
 	# THIS WILL CHANGE WHEN WE HAVE LEVELS
+	$Music.stop()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
