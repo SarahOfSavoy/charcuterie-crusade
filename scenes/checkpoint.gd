e1a31d90f180774d22f9f248466c3b30b13823cc
sgastuel <segastwirth@icloud.com>
2025-03-31 14:31:04 -0500

    Fixed checkpoints for tutorial.tscn

--- scenes/checkpoint.gd
+++ scenes/checkpoint.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-# Current save state score
 var score = 0
 
 func _on_body_entered(body: Node2D) -> void:
-	Globals.checkpoint = self
-	score = Globals.score
+	if body.name == "Player" or body.is_in_group("player"):
+		Globals.checkpoint = self
+		score = Globals.score
+		print("Checkpoint saved at:", global_position)
--- scenes/level_end.gd
+++ scenes/level_end.gd
@@ -1,27 +1,20 @@
 extends CanvasLayer
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"Level End Screen/Score Label".text = "Score: " + str(Globals.score)
 
+
 # When the restart button is pressed
 func _on_restart_pressed() -> void:
 	Globals.is_paused = false
-	
-	# Get current scene path
-	var current_scene = get_tree().current_scene.scene_file_path
-	
-	# Only restore checkpoint data if in tutorial
-	if "tutorial" in current_scene.to_lower():
-		Globals.score = Globals.checkpoint.score
-		var player = get_parent().get_parent()
-		player.heal(100 - Globals.health)
-		player.position = Globals.checkpoint.position
-	
-	# Reload the current scene to reset everything
-	get_tree().reload_current_scene()
+	Globals.score = Globals.checkpoint.score
+	var player = get_parent().get_parent()
+	player.take_damage(Globals.health - 100)
+	player.position = Globals.checkpoint.position
 	queue_free()
-
+	
 # When the continue button is pressed
 func _on_continue_pressed() -> void:
 	get_tree().quit()
