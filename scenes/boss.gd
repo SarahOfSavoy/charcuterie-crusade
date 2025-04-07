5338ddc0a6c7ab6745d847e92ba7290eec9d42b5
sgastuel <segastwirth@icloud.com>
2025-04-06 21:23:47 -0500

    New backgrounds, boss music, boss respawn

--- scenes/boss.gd
+++ scenes/boss.gd
@@ -2,16 +2,26 @@ extends Node2D
 
 @export var max_health: int = 100
 @export var hits_to_phase2: int = 10
+
 var current_hits: int = 0
 var is_phase2: bool = false
-
 var health: int = max_health
 
 @onready var fist_spawner = $FistSpawner
 @onready var knife_spawner = $KnifeSpawner
+@onready var music_player: AudioStreamPlayer2D = $Music
+@onready var victory_label: Label = get_node_or_null("/root/BossLevel/VictoryLabel")
+
 
 func _ready():
+		
 	health = max_health
+	# Hide the victory label at start
+	if victory_label:
+		print('victorylabel')
+		victory_label.visible = false
+
+	# Start Phase 1
 	fist_spawner.start_spawning()
 
 func on_boss_damaged(damage):
@@ -31,9 +41,8 @@ func start_phase2():
 	is_phase2 = true
 	print("=== PHASE 2 ACTIVATED ===")
 	
-	# Phase 2 adjustments
-	fist_spawner.current_spawn_interval *= 0.6  # 40% faster spawning
-	fist_spawner.current_fist_speed *= 1.5      # 50% faster movement
+	fist_spawner.current_spawn_interval *= 0.6
+	fist_spawner.current_fist_speed *= 1.5
 	
 	if knife_spawner:
 		knife_spawner.start_spawning()
@@ -42,4 +51,9 @@ func start_phase2():
 
 func die():
 	print("Butcher King defeated!")
+
+	if victory_label:
+		print("victorylabel")
+		victory_label.visible = true
+
 	queue_free()
--- scenes/level_end.gd
+++ scenes/level_end.gd
@@ -5,14 +5,25 @@ extends CanvasLayer
 func _ready() -> void:
 	$"Level End Screen/Score Label".text = "Score: " + str(Globals.score)
 
+func is_boss_level() -> bool:
+	return get_tree().current_scene.name == "BossLevel"
 
 # When the restart button is pressed
 func _on_restart_pressed() -> void:
 	Globals.is_paused = false
-	Globals.score = Globals.checkpoint.score
-	var player = get_parent().get_parent()
-	player.take_damage(Globals.health - 100)
-	player.position = Globals.checkpoint.position
+	print(is_boss_level())
+	if is_boss_level():
+		print("Restarting boss level from beginning.")
+		# Don't use checkpoint — just reload the scene
+		get_tree().reload_current_scene()
+	else:
+		# Standard level: reset to checkpoint
+		if Globals.checkpoint != null:
+			Globals.score = Globals.checkpoint.score
+			var player = get_parent().get_parent()
+			player.take_damage(Globals.health - 100)
+			player.position = Globals.checkpoint.position
+
 	queue_free()
 	
 # When the continue button is pressed
